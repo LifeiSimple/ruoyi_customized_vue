0f00d63b1d4260c549c33190fe5982d1feb049a5
wangjx <wangjiaxiang97@foxmail.com>
2025-03-02 21:50:40 +0800

    build:集成 dayjs 并更新相关文档
    - 在 main.js 中引入 dayjs 并挂载到 Vue 原型
    - 在 package.json 中添加 dayjs 依赖
    - 更新 README.md，增加 dayjs 安装和使用说明

--- src/main.js
+++ src/main.js
@@ -37,6 +37,8 @@ import DictTag from '@/components/DictTag'
 import VueMeta from 'vue-meta'
 // 字典数据组件
 import DictData from '@/components/DictData'
+// dayjs
+import dayjs from "dayjs";
 
 // 全局方法挂载
 Vue.prototype.getDicts = getDicts
@@ -48,6 +50,7 @@ Vue.prototype.selectDictLabel = selectDictLabel
 Vue.prototype.selectDictLabels = selectDictLabels
 Vue.prototype.download = download
 Vue.prototype.handleTree = handleTree
+Vue.prototype.dayjs = dayjs;
 
 // 全局组件挂载
 Vue.component('DictTag', DictTag)
